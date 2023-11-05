e5365282926fca4b4d46477a52800e1740f4fdf1
Eliška Krátká <eliska.kratka@gendigital.com>
2023-11-05 11:04:16 +0100

    project setup

---
+++ RocketApp/RocketApp/RocketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketAppApp.swift
+//  RocketApp
+//
+//  Created by Krátká Eliška on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RocketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
