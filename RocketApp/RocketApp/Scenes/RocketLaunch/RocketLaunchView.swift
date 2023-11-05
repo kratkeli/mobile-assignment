326c3b43d545d58fe5eb0293fcab39e0b96426da
Eliška Krátká <eliska.kratka@gendigital.com>
2023-11-05 15:20:16 +0100

    Added plain RocketLaunchView

---
+++ RocketApp/RocketApp/Scenes/RocketLaunch/RocketLaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  RocketLaunchView.swift
+//  RocketApp
+//
+//  Created by Krátká Eliška on 05.11.2023.
+//
+
+import SwiftUI
+
+struct RocketLaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RocketLaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        RocketLaunchView()
+    }
+}
