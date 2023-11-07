efaa575a629041403eab0eaa578d56116f514dd4
Eliška Krátká <eliska.kratka@gendigital.com>
2023-11-07 12:01:11 +0100

    Added RocketAPIService and RocketDetail to Localization

--- RocketApp/RocketApp/SupportingFiles/Strings/Localization.swift
+++ RocketApp/RocketApp/SupportingFiles/Strings/Localization.swift
@@ -8,7 +8,18 @@
 import Foundation
 
 enum AppStrings {
+    enum RocketAPIService {
+        static let apiURL = "https://api.spacexdata.com/v3/rockets"
+    }
+    
     enum RocketList {
         static let title = "Rockets"
+    }
+    
+    enum RocketDetail {
+        static let launch = "Launch"
+        static let overview = "Overview"
+        static let parameters = "Parameters"
+        static let photos = "Photos"
     }
 }
