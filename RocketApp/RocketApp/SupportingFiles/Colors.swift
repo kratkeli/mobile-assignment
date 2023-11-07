31b66409af2e2159cc353c24d1c670918fa3f8c7
Eliška Krátká <eliska.kratka@gendigital.com>
2023-11-07 17:16:46 +0100

    Added colors

---
+++ RocketApp/RocketApp/SupportingFiles/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  RocketApp
+//
+//  Created by Krátká Eliška on 07.11.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let rocketPink = Color(#colorLiteral(red: 0.9503846765, green: 0.3185824752, blue: 0.5299354792, alpha: 1))
+    static let stageGray = Color(#colorLiteral(red: 0.9647058845, green: 0.9647058845, blue: 0.9647058845, alpha: 1))
+}
